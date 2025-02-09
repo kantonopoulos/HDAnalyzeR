d4ed5e31a7ef9133fe33bba88b2d1a13e8640b4c
Konstantinos Antonopoulos <k.antono@outlook.com>
2025-02-09 12:43:42 +0100

    Increase the file size in app from 5MB to 30MB

--- hdanalyzer_app/app.R
+++ hdanalyzer_app/app.R
@@ -257,7 +257,7 @@ ui <- navbarPage(
 
 # Define server ----------------------------------------------------------------
 server <- function(input, output, session) {
-
+  options(shiny.maxRequestSize=30*1024^2)
   # Input data -----------------------------------------------------------------
   # Reactive values for data and metadata
   data <- reactive({
